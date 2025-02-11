482e2125577f42e7217090fca15d2fe00087e034
Josh Rabbani <jar188@duke.edu>
2025-02-10 23:27:36 -0500

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -5,14 +6,12 @@ import Home from './pages/Home';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-white">
+      <div>
         <Header />
-        <main className="max-w-6xl mx-auto px-6 py-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Add more routes as we create them */}
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes as we create them */}
+        </Routes>
       </div>
     </Router>
   );
